0638e79d465b8312a2746949e74e7da81f78a51f
sudhanshu2005270 <67102533+sudhanshu2005270@users.noreply.github.com>
2020-06-18 17:27:29 +0530

    Create transpose_matrix.c

---
+++ transpose_matrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i,j;
+    printf("Enter the order of the matrix: ");
+    scanf("%d%d",&m,&n);
+    {
+        int arr[m][n],arrt[n][m],sum=0;
+        printf("Enter the elements of the matrix: \n");
+        for(i=0;i<m;i++)
+            for(j=0;j<n;j++)
+                scanf("%d",(*(arr+i)+j));
+        printf("The matrix entered by you is: \n");
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                printf("%d\t",*(*(arr+i)+j));
+
+            }
+            printf("\n");
+        }
+        for(i=0;i<m;i++)
+            for(j=0;j<n;j++)
+                *(*(arrt+j)+i)=*(*(arr+i)+j);
+        printf("The transpose of the matrix is: \n");
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                printf("%d\t",*(*(arrt+i)+j));
+
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
